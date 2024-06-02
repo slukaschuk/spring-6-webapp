508427536874bd1b6cec19ca644c36536f097fd6
serhii lukashchuk <sergey.lukaschuk@gmail.com>
2024-06-02 13:21:34 +0800

    Assignment 2 result

---
+++ src/main/java/guru/springframework/spring6webapp/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package guru.springframework.spring6webapp.controllers;
+
+import guru.springframework.spring6webapp.services.AuthorService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @RequestMapping("/authors")
+    public String getAllAuthors(Model model) {
+        model.addAttribute("authors", authorService.findAll());
+
+        return "authors";
+    }
+}

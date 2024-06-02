f4d319d6a40bed46149a58f3af7b95caeb3ec7ff
serhii lukashchuk <sergey.lukaschuk@gmail.com>
2024-06-02 12:21:41 +0800

    Assignment 1 result

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,10 +17,12 @@ public class BootstrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -51,11 +55,28 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
 
+        Publisher publisherWA = new Publisher();
+        publisherWA.setPublisherName("4 books");
+        publisherWA.setAddress("WA East Perth 6000");
+        publisherWA.setCity("Perth");
+        publisherWA.setZip("6000");
+        publisherWA.setState("WA");
+
+        Publisher publisherUkraine = new Publisher();
+        publisherUkraine.setPublisherName("Kyiv Publisher Ukraine");
+        publisherUkraine.setAddress("Nezalezhnosti street 1");
+        publisherUkraine.setCity("Kyiv");
+        publisherUkraine.setZip("7002");
+        publisherUkraine.setState("Kyiv");
+
+        publisherRepository.save(publisherUkraine);
+        publisherRepository.save(publisherWA);
+
+
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
-
-
+        System.out.println("Publisher Count: " + publisherRepository.count());
     }
 }
 
